ee28bcd97f3349cd7d47eccd06ddd1efb5010b09
Gautam Sharma <31288706+gautamsharma480@users.noreply.github.com>
2025-11-20 15:38:10 -0500

    Update reciprocal matrix extraction method
    Extract reciprocal matrix from 'scf.out' instead of using a hardcoded matrix.

--- kpath_distance.py
+++ kpath_distance.py
@@ -2,8 +2,21 @@ import numpy as np
 
 alat = 1 # it is taken in fraction units
 
-# you need reciprocal matrix below (copy it from scf.out)
-reciprocal_mat  =  np.array([[1,0,0],[0,1,0],[0,0,1]]) # simple cubic
+
+# you need reciprocal matrix below (extract it from scf.out)
+f=open("scf.out", "r")
+    for line in f:
+        if "b(1) =" in line:
+            b1 = list(map(float, line.split()[3:6]))
+        if "b(2) =" in line:
+            b2 = list(map(float, line.split()[3:6]))
+        if "b(3) =" in line:
+            b3 = list(map(float, line.split()[3:6]))
+
+reciprocal_mat = np.array([b1, b2, b3])
+
+
+# reciprocal_mat  =  np.array([[1,0,0],[0,1,0],[0,0,1]]) # simple cubic
 
 # You need a text file with kpts in fractional coordinates: only float values are accepted in np.loadtxt
 kpts = np.loadtxt("kpts.dat")
